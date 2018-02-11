f96d23c2dda1fae7d6f1b4984ff84cf816e87733
Ilia <ilia.pohorielov@gmail.com>
2018-02-11 16:27:48 +0200

    END STYLE LANDING PAGE

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,38 @@ $(document).ready(function() {
         arrows: false,
         dots: true
     });
+    $('.js-slider-testimonial').slick({
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1255,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+    function heightEL(el) {
+        var elH = el;
+        var maxHeight = 0;
+        for (var i = 0; i < elH.length; ++i) {
+            elH[i].style.height = "";
+            if (maxHeight < elH[i].clientHeight) {
+                maxHeight = elH[i].clientHeight;
+            }
+        }
+        for (var i = 0; i < elH.length; ++i) {
+            elH[i].style.height = maxHeight + "px";
+        }
+    }
+    if ($(window).width() >= 767) {
+        heightEL($('.features .feature-item'));
+    }
+    $('.js_validate [type="submit"]').on("click", function () {
+        return validate($(this).parents(".js_validate"));
+    });
+    $('.js-mobile-menu').on('click', function () {
+       $(this).toggleClass('active');
+       $(this).parents('header').find('.main-menu').slideToggle();
+    });
 });
